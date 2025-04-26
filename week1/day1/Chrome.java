916864b82950785d1392b612654fdff9b528ebfd
RajashreeT15 <rajashreethangaraj15@gmail.com>
2025-04-26 20:44:51 +0200

    Add files via upload
    Rajashree-Week1day1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		// version of type float with a value of 91.0f
+		float version = 91.0f;
+		//developer of type String with a value of "Google"
+		String developer = "Google";
+		//isBeta of type boolean with a value of false
+		boolean isBeta = false;
+		//releaseYear of type int with a value of 2008
+		int releaseYear = 2008;
+		//shortcutKey of type char with a value of 'C'
+		char shortcutKey = 'C';
+
+		System.out.println("Version: " + version);
+		System.out.println("Developer: " + developer);
+		System.out.println("IsBeta: " + isBeta);
+		System.out.println("ReleaseYear: " + releaseYear);
+		System.out.println("ShortcutKey: " + shortcutKey);
+
+	}
+
+}
